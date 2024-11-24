72a152554166fdcb4d75d5a829b2f007471a329a
Dat Mai <datmtse161965@fpt.edu.vn>
2024-11-24 10:14:25 +0700

    add checkout flow

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ import { v2 as cloudinary } from "cloudinary";
 
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
-import RestaurantRoute from "./routes/RestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -20,8 +21,11 @@ cloudinary.config({
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
+
+app.use(express.json());
 
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
@@ -29,7 +33,8 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
-app.use("/api/restaurant", RestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
+app.use("/api/order", orderRoute);
 
 app.listen(7000, () => {
   console.log(`Server is running on localhost:7000`);
